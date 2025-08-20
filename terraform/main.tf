499756d58025c91c6757429a977f5f8816f82ec4
Arnold <arnoldstar77@gmail.com>
2025-08-20 09:31:25 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,21 @@
+provider "aws" {
+  region = var.aws_region
+}
 
+module "network" {
+  source = "./modules/network"
+  project_name = var.project_name
+}
+
+module "ecs" {
+  source = "./modules/ecs"
+  vpc_id          = module.network.vpc_id
+  public_subnets  = module.network.public_subnets
+  project_name = var.project_name
+  cpu          = var.cpu
+  memory       = var.memory
+  container_port = var.container_port
+  image_uri    = "${module.ecr.repository_url}:latest"  # using repo from ECR module
+  aws_region   = var.aws_region
+  env          = var.env
+}
