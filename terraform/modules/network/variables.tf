8e238ca9b567d711dedcf235098178f4c90cc5c3
Arnold <arnoldstar77@gmail.com>
2025-08-20 12:40:01 +0530

    Create variables.tf

---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  description = "Project name used for tagging"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
