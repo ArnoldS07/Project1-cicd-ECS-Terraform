e609e11efcba087faed0df89078b6b76011fbae7
Arnold <arnoldstar77@gmail.com>
2025-08-20 12:04:54 +0530

    Update variables.tf

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -7,15 +7,14 @@ variable "aws_region" {
   default = "ap-south-1"
 }
 
-variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+variable "vpc_id" {
+  type = string
 }
 
-variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnets" {
+  type = list(string)
 }
+
 
 variable "container_port" {
   type    = number
