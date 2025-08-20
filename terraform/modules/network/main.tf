edd3964d6053f0435611da7b260932af9bac3ca1
Arnold <arnoldstar77@gmail.com>
2025-08-20 12:29:05 +0530

    Update main.tf

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -35,3 +35,11 @@ resource "aws_route_table_association" "public" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.public.id
 }
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "public_subnets" {
+  value = [for s in aws_subnet.public : s.id]
+}
