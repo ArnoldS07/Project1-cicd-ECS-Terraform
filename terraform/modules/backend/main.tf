c58d592d5d1cc7ecf874b1d0143ddc5eb500ca16
Arnold <arnoldstar77@gmail.com>
2025-08-20 11:16:56 +0530

    Update main.tf

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -1,6 +1,6 @@
 # S3 bucket resource
 resource "aws_s3_bucket" "backend" {
-  bucket = "codewithmuh-terraform-state-backend-image-99"
+  bucket = "arnold-terraform-ecs-proj007"
 }
 
 # Setting S3 bucket ownership controls
