99de0f7d9c2b3321e565706363649721daf7e892
Arnold <arnoldstar77@gmail.com>
2025-08-20 11:12:45 +0530

    Add files via upload

--- terraform/modules/backend/main.tf
+++ terraform/modules/backend/main.tf
@@ -1,9 +1,44 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-backend777"       
-    key            = "ecs-fargate-demo/terraform.tfstate" 
-    region         = "ap-south-1"                    
-    dynamodb_table = "terraform-state-lock-table" 
-    encrypt        = true
+# S3 bucket resource
+resource "aws_s3_bucket" "backend" {
+  bucket = "codewithmuh-terraform-state-backend-image-99"
+}
+
+# Setting S3 bucket ownership controls
+resource "aws_s3_bucket_ownership_controls" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
   }
+}
+
+# S3 bucket ACL
+resource "aws_s3_bucket_acl" "backend" {
+  depends_on = [aws_s3_bucket_ownership_controls.backend]
+  bucket     = aws_s3_bucket.backend.id
+  acl        = "private"
+}
+
+# Setting S3 bucket encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "backend" {
+  bucket = aws_s3_bucket.backend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# DynamoDB table resource
+resource "aws_dynamodb_table" "backend" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  hash_key = "LockID"
 }
